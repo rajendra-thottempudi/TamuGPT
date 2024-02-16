3da224b27861c7d6241914026bbb0a18bb278a76
Rajendra Thottempudi <nanirajendra1@gmail.com>
2024-02-16 17:04:02 -0600

    updated route

--- api/chat.py
+++ api/chat.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def home():
     return "Flask Vercel Example - Hello World", 200
 
-# Basic flask app to chat with a user
 # Route to get a string as a response after taking input from the user
 @app.route('/<message>', methods=['GET'])
 def get_book(message: str):
