f58bda509aa460741a1014b49ff6369e697be18a
Rajendra Thottempudi <nanirajendra1@gmail.com>
2024-02-16 17:26:19 -0600

    updating cors

--- api/chat.py
+++ api/chat.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
-cors = CORS(app)
 app = Flask(__name__)
+cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
