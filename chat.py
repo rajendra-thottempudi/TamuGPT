07aab65d9f35303775ffa101d0a9f5c01ba39284
Rajendra Thottempudi <nanirajendra1@gmail.com>
2024-02-16 16:14:23 -0600

    Basic endpoint check

---
+++ chat.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Basic flask app to chat with a user
+# Route to get a string as a response after taking input from the user
+@app.route('/<message:>', methods=['GET'])
+def get_book(message: str):
+    return "Welcome"
+
+if __name__ == '__main__':
+    app.run()
