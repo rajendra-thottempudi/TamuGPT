f32923e8a1b62fbf3bf1ebb6698f28785754588c
Rajendra Thottempudi <nanirajendra1@gmail.com>
2024-02-16 17:38:58 -0600

    fixed typo

--- api/chat.py
+++ api/chat.py
@@ -4,9 +4,9 @@ from flask import jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
 @cross_origin()
