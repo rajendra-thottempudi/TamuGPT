209de3051f7ac00722691c9bbf01ff4b06ed6a57
Rajendra Thottempudi <nanirajendra1@gmail.com>
2024-02-16 17:23:14 -0600

    adding cors

--- api/chat.py
+++ api/chat.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
+cors = CORS(app)
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def home():
     return "Flask Vercel Example - Hello World", 200
 
 # Route to get a string as a response after taking input from the user
 @app.route('/<message>', methods=['GET'])
+@cross_origin()
 def get_book(message: str):
     return "Welcome"
 
