44741e9a7e4538aa7c2b50912880fc748178441a
Rajendra Thottempudi <nanirajendra1@gmail.com>
2024-04-23 16:34:16 -0500

    changed code placement

--- api/data.py
+++ api/data.py
@@ -1,61 +1,61 @@
-import re
-# from transformers import AutoTokenizer, AutoModel
-# import torch
-from pinecone import Pinecone
-import openai
-import os
+# import re
+# # from transformers import AutoTokenizer, AutoModel
+# # import torch
+# from pinecone import Pinecone
+# import openai
+# import os
 
-# def generateEmbeddings(texts):
-#   # Load model from HuggingFace Hub
-#   tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-small-en-v1.5')
-#   model = AutoModel.from_pretrained('BAAI/bge-small-en-v1.5')
-#   model.eval()
-#   # Tokenize sentences
-#   encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
-#   # for s2p(short query to long passage) retrieval task, add an instruction to query (not add instruction for passages)
-#   # encoded_input = tokenizer([instruction + q for q in queries], padding=True, truncation=True, return_tensors='pt')
-#   # Compute token embeddings
-#   with torch.no_grad():
-#     model_output = model(**encoded_input)
-#     # Perform pooling. In this case, cls pooling.
-#     sentence_embeddings = model_output[0][:, 0]
-#   # normalize embeddings
-#   sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
-#   # print("Sentence embeddings:", sentence_embeddings)
-#   return sentence_embeddings
+# # def generateEmbeddings(texts):
+# #   # Load model from HuggingFace Hub
+# #   tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-small-en-v1.5')
+# #   model = AutoModel.from_pretrained('BAAI/bge-small-en-v1.5')
+# #   model.eval()
+# #   # Tokenize sentences
+# #   encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
+# #   # for s2p(short query to long passage) retrieval task, add an instruction to query (not add instruction for passages)
+# #   # encoded_input = tokenizer([instruction + q for q in queries], padding=True, truncation=True, return_tensors='pt')
+# #   # Compute token embeddings
+# #   with torch.no_grad():
+# #     model_output = model(**encoded_input)
+# #     # Perform pooling. In this case, cls pooling.
+# #     sentence_embeddings = model_output[0][:, 0]
+# #   # normalize embeddings
+# #   sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
+# #   # print("Sentence embeddings:", sentence_embeddings)
+# #   return sentence_embeddings
 
-def getEmbedding(sentence):
-  secret_key = os.environ.get('SECRET_KEY')
-  openai.api_key = secret_key
-  query_embedding_response = openai.embeddings.create(
-        model="text-embedding-3-small",
-        input= sentence,
-  )
+# def getEmbedding(sentence):
+#   secret_key = os.environ.get('SECRET_KEY')
+#   openai.api_key = secret_key
+#   query_embedding_response = openai.embeddings.create(
+#         model="text-embedding-3-small",
+#         input= sentence,
+#   )
 
-  q_embed = query_embedding_response.data[0].embedding
-  return q_embed
+#   q_embed = query_embedding_response.data[0].embedding
+#   return q_embed
 
 
-def getPineConeIndex():
-  apiKey = "6aa7b0bc-9d95-46c4-b9e1-88b3b659a554"
-  index_name = "tamuembed"
-  pc = Pinecone(api_key=apiKey)
-  index = pc.Index(index_name)
-  return index
+# def getPineConeIndex():
+#   apiKey = "6aa7b0bc-9d95-46c4-b9e1-88b3b659a554"
+#   index_name = "tamuembed"
+#   pc = Pinecone(api_key=apiKey)
+#   index = pc.Index(index_name)
+#   return index
 
-def getKClosest(question):
-  db_index = getPineConeIndex()
-  query_vector = [question]
-  embed = getEmbedding(query_vector)
-  closest = db_index.query(vector = embed, top_k = 20, include_metadata=True)  
-  # print(closest)
-  closest_strings = []
+# def getKClosest(question):
+#   db_index = getPineConeIndex()
+#   query_vector = [question]
+#   embed = getEmbedding(query_vector)
+#   closest = db_index.query(vector = embed, top_k = 20, include_metadata=True)  
+#   # print(closest)
+#   closest_strings = []
   
-  for result in closest['matches']:
-    text = result['metadata']['text']
-    closest_strings.append(text)
-    # print(f"id : {result['id']}")
+#   for result in closest['matches']:
+#     text = result['metadata']['text']
+#     closest_strings.append(text)
+#     # print(f"id : {result['id']}")
 
-  return closest_strings
+#   return closest_strings
 
 
--- api/llm.py
+++ api/llm.py
@@ -1,38 +1,38 @@
-from data import getKClosest
-import openai  # for calling the OpenAI API
-import pandas as pd  # for storing text and embeddings data
-import tiktoken  # for counting tokens
-import os
+# from data import getKClosest
+# import openai  # for calling the OpenAI API
+# import pandas as pd  # for storing text and embeddings data
+# import tiktoken  # for counting tokens
+# import os
 
-def num_tokens(text: str, model: str = "gpt-3.5-turbo") -> int:
-    """Return the number of tokens in a string."""
-    encoding = tiktoken.encoding_for_model(model)
-    # print(len(encoding.encode(text)))
-    return len(encoding.encode(text))
+# def num_tokens(text: str, model: str = "gpt-3.5-turbo") -> int:
+#     """Return the number of tokens in a string."""
+#     encoding = tiktoken.encoding_for_model(model)
+#     # print(len(encoding.encode(text)))
+#     return len(encoding.encode(text))
 
-def query_message(
-    query: str,
-    model: str,
-    token_budget: int
-) -> str:
-    """Return a message for GPT, with relevant source texts pulled from a dataframe."""
-    closest_strings = getKClosest(query)
-    introduction = 'Use the below Texas A&M Information to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."'
-    question = f"\n\nQuestion: {query}"
-    message = introduction
-    for string in closest_strings:
-        next_article = f'\n\n TexasA&M Information :\n"""\n{string}\n"""'
-        if (
-            num_tokens(message + next_article + question, model=model)
-            > token_budget
-        ):
-            break
-        else:
-            message += next_article
-    return message + question
+# def query_message(
+#     query: str,
+#     model: str,
+#     token_budget: int
+# ) -> str:
+#     """Return a message for GPT, with relevant source texts pulled from a dataframe."""
+#     closest_strings = getKClosest(query)
+#     introduction = 'Use the below Texas A&M Information to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."'
+#     question = f"\n\nQuestion: {query}"
+#     message = introduction
+#     for string in closest_strings:
+#         next_article = f'\n\n TexasA&M Information :\n"""\n{string}\n"""'
+#         if (
+#             num_tokens(message + next_article + question, model=model)
+#             > token_budget
+#         ):
+#             break
+#         else:
+#             message += next_article
+#     return message + question
 
 
-def ask(
+# def ask(
     query: str,
     model: str = "gpt-3.5-turbo",
     token_budget: int = 4000,
